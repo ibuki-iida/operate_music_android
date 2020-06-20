14797c7b9217431d91d60ac56193dac39e65db87
飯田惟吹 <ibuki.iida@topgate.co.jp>
2020-06-20 22:39:18 +0900

    feat: プロジェクトの作成

--- operate_music_for_android/app/src/main/java/com/example/operate_music_for_android/MainActivity.kt
+++ operate_music_for_android/app/src/main/java/com/example/operate_music_for_android/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.example.operate_music
+package com.example.operate_music_for_android
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
